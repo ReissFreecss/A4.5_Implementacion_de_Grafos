cd06299e89117c311475d9693a5aa688325db06c
darkd <darkd@Carlos-Calderon>
2021-11-15 11:29:18 -0600

    1. Se agregaron la clase principal A45_GRAFOS es el main 2. Se agrego la clase lienzo y nodo 3. En esta secion simplemente se dibujan los circulos para los grafos al    hacer clic

--- src/a4/pkg5_grafos/A45_GRAFOS.java
+++ src/a4/pkg5_grafos/A45_GRAFOS.java
@@ -2,6 +2,8 @@
  */
 package a4.pkg5_grafos;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author darkd
@@ -12,7 +14,11 @@ public class A45_GRAFOS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame ventana = new JFrame("GRAFO");
+        ventana.add(new Lienzo());
+        ventana.setSize(600, 600);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
     }
     
 }
